6c2dca0c01c1c7a47f8db6f1ff86b858a0a3e1f1
medhanies <medhaniesolomon@yahoo.com>
2023-04-06 13:42:48 -0500

    working with structs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,117 @@
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// fn main() {
+//     let rect1 = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//       area(rect1)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//   dimensions.0 * dimensions.1
+// }
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//       width: dbg!(30 * scale),
+//         height: 50,
+//     };
+
+//   dbg!(&rect1);
+// }
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//       height: u32,
+//   }
+  
+//   fn main() {
+//     let rect1 = Rectangle {
+//           width: 30,
+//           height: 50,
+//       };
+  
+//       println!("rect1 is {:#?}", rect1);
+
+//       println!(
+//           "The area of the rectangle is {} square pixels.",
+//           area(&rect1)
+//       );
+//   }
+  
+//   fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+//   }
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+  fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
+
+  fn square(size: u32) -> Self  {
+    Self  {
+        width: size,
+        height: size,
+    }
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let sq = Rectangle::square(3);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("square has area of {}", sq.area());
+
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+      rect1.area()
+    );
 }
