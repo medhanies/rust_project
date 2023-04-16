e7b26a577a747ef089de1c33b3e1a02de89803d3
medhanies <medhaniesolomon@yahoo.com>
2023-04-15 22:14:25 -0500

    modify owner

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,8 @@
 // fn main() {
 //     let mut s = String::from("hello");
-    
+
+//     println!("{s}");
+
 //     s.push_str(", world!"); // push_str() appends a literal to a String
 
 //     println!("{s}"); // this will print `hello, world!`
@@ -10,21 +12,22 @@
 //     let x = 5;
 //     let y = x;
 
-//     print!("{x} = x, {y} = y");
+//     println!("{x} = x, {y} = y");
 
 //     let s1 = String::from("hello");
 //     let s2 = s1.clone();
+//     // let s2 = &s1;
 
 //     println!("{s1}, world!");
 //     print!("{s1} = s1, {s2} = s2");
 // }
 
 // fn main() {
-//     let s = String::from("hello");  // s comes into scope
+//     let mut s = String::from("hello");  // s comes into scope
 
 //     takes_ownership(s);             // s's value moves into the function...
-//     // let s = String::from("hi");                                   // ... and so is no longer valid here
-
+//     s = String::from("hi");                                   // ... and so is no longer valid here
+//     takes_ownership(s);
 //     let x = 5;                      // x comes into scope
 
 //     makes_copy(x);                  // x would move into the function,
@@ -45,12 +48,12 @@
 
 // fn main() {
 //     let s1 = gives_ownership();         // gives_ownership moves its return
-//      print!("{s1}");                                   // value into s1
+//      println!("{s1}");                                   // value into s1
 
 //     let s2 = String::from("hello");     // s2 comes into scope
-//     print!("{s2}");
+//     println!("{s2}");
 //     let s3 = takes_and_gives_back(s2);  // s2 is moved into
-//     print!("{s3}");                                    // takes_and_gives_back, which also
+//     println!("{s3}");                                    // takes_and_gives_back, which also
 //                                         // moves its return value into s3
 // } // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
 //   // happens. s1 goes out of scope and is dropped.
@@ -103,6 +106,7 @@
 //     let mut s = String::from("hello");
 
 //     change(&mut s);
+//     print!("{s}");
 // }
 
 // fn change(some_string: &mut String) {
@@ -188,66 +192,66 @@
 //   }
 // }
 
-// fn main() {
-//     let my_string = String::from("hello world");
-
-//     // `first_word` works on slices of `String`s, whether partial
-//     // or whole.
-//     let word = first_word(&my_string[0..6]);
-
-//     print!("{word}");
-
-//     let word = first_word(&my_string[..]);
-
-//     print!("{word}");
-//     // `first_word` also works on references to `String`s, which
-//     // are equivalent to whole slices of `String`s.
-//     let word = first_word(&my_string);
-
-//     print!("{word}");
-
-//     let my_string_literal = "hello world";
-
-//     // `first_word` works on slices of string literals,
-//     // whether partial or whole.
-//     let word = first_word(&my_string_literal[0..6]);
-
-//     print!("{word}");
-
-//     let word = first_word(&my_string_literal[..]);
-
-//     print!("{word}");
-//     // Because string literals *are* string slices already,
-//     // this works too, without the slice syntax!
-//     let word = first_word(my_string_literal);
-
-//     print!("{word}");
-// }
-
-// fn first_word(s: &str) -> &str {
-//     let bytes = s.as_bytes();
-  
-//       for (i, &item) in bytes.iter().enumerate() {
-//         if item == b' ' {
-//               return &s[0..i];
-//           }
-//       }
-  
-//     &s[..]
-//   }
-
 fn main() {
+    let my_string = String::from("hello world");
 
-    let a = [1,2,3,4,5];
+    // `first_word` works on slices of `String`s, whether partial
+    // or whole.
+    let word = first_word(&my_string[0..6]);
 
-    // let slice = &a[1..3];
+    print!("{word}");
 
-    let slice = &a;
+    let word = first_word(&my_string[..]);
 
-    // assert_eq!(slice, &[2, 3]);
+    print!("{word}");
+    // `first_word` also works on references to `String`s, which
+    // are equivalent to whole slices of `String`s.
+    let word = first_word(&my_string);
 
-    // println!("{:?}", slice);
+    print!("{word}");
 
-    assert_eq!(slice, &[1,2,3,4,5]);
-    println!("{:?}", slice);
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals,
+    // whether partial or whole.
+    let word = first_word(&my_string_literal[0..6]);
+
+    print!("{word}");
+
+    let word = first_word(&my_string_literal[..]);
+
+    print!("{word}");
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+
+    print!("{word}");
 }
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+  
+      for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+              return &s[0..i];
+          }
+      }
+  
+    &s[..]
+  }
+
+// fn main() {
+
+//     let a = [1,2,3,4,5];
+
+//     // let slice = &a[1..3];
+
+//     let slice = &a;
+
+//     // assert_eq!(slice, &[2, 3]);
+
+//     // println!("{:?}", slice);
+
+//     assert_eq!(slice, &[1,2,3,4,5]);
+//     println!("{:?}", slice);
+// }
